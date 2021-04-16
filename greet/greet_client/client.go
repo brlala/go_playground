6cdc8c27d283710dd6fb7a073f7e94d92a21e048
liheng <liheng@pand.ai>
2021-04-16 22:04:28 +0800

    completed boiler plate code

---
+++ greet/greet_client/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"go_playground/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	// grpc by default has SSL, this line is to remove it, disable SSL and open a connection to the port
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	// defer keyword defer this code until the very end of the code
+	defer cc.Close()
+
+	// create a new client
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go_playground/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	//port binding
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	//create grpc server
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	// binding the port to the grpc server
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
