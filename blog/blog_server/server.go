2e6b71bc91451261482c7c8b98c8ddf316930bd1
liheng <liheng@pand.ai>
2021-04-17 23:19:21 +0800

    adding graceful shut down

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,1 +1,68 @@
-package blog_server
+package main
+
+import (
+	"fmt"
+	"go_playground/blog/blogpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+type server struct {
+	blogpb.UnimplementedBlogServiceServer
+}
+
+func main() {
+	fmt.Println("Blog Service started")
+
+	//port binding
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	tls := false
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, sslErr := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if sslErr != nil {
+			log.Fatalf("Failed loading certificates: %v", sslErr)
+		}
+		//ssl options
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	//create grpc server
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	//adding reflection to the server, $ evans -p 50051 -r
+	reflection.Register(s)
+
+	//adding hooks to properly shutdown server
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until a signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Stopping the listener")
+	lis.Close()
+	fmt.Println("End of Program")
+}
