6f1492efa3d9acf3cc26bf4bc11bcbb55882680a
liheng <liheng@pand.ai>
2021-04-17 23:12:50 +0800

    adding blog CRUD with mongodb

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative greet\greetpb\greet.proto
 #protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
-#protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative blog\blogpb\blog.proto
